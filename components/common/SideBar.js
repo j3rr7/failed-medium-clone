1c2207dd0d51717a6cda328584dfc6789aa3b9f9
Volffied <70651519+Volffied@users.noreply.github.com>
2022-03-25 13:36:17 +0700

    detail page:(

---
+++ components/common/SideBar.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import logo from '../../public/assets/images/logo.png'
+import { Icon } from '@iconify/react';
+import Image from 'next/image';
+
+export default function SideBar() {
+  return (
+    <div className="h-screen sticky top-0 border-r-1">
+      <div className="mt-12 mr-4">
+        <div className="px-2">
+          <Image src={logo} width={30} height={30}></Image>
+        </div>
+      </div>
+
+      <div className="mt-52 mr-4">
+        <div className="mt-3 py-3 px-2">
+          <Icon icon="dashicons:admin-home" width="30" height="30"/>
+        </div>
+        <div className="mt-3 py-3 px-2">
+          <Icon icon="fluent:alert-16-regular" width="30" height="30" />
+        </div>
+        <div className="mt-3 py-3 px-2">
+          <Icon icon="fluent:bookmark-multiple-20-regular" width="30" height="30" />
+        </div>
+        <div className="mt-3 py-3 px-2">
+          <Icon icon="majesticons:paper-fold-text-line" width="30" height="30" />
+        </div>
+
+        <div className="my-10 border-b-1 opacity-30"></div>
+
+        <div className="mt-3 py-3 px-2">
+          <Icon icon="dashicons:welcome-write-blog" width="30" height="30" />
+        </div>
+      </div>
+
+      <div className="mt-52 mr-4">
+        <div className="py-2 px-2">
+          <Icon icon="bxs:user-circle" width="30" height="30" />
+        </div>
+      </div>
+    </div>
+  )
+}
---
+++ pages/search.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Member from '../components/common/Member'
+export default function search() {
+  return (
+    <>
+      <Member/>
+    </>
+  )
+}
